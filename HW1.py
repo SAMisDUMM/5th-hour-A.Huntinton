191a97c66e328cd90788cdac982701f32f1f164c
Ashtin Gage Huntington <ah128195@stundent.wpsok.org>
2024-08-22 13:04:17 -0500

    Added HW2

--- HW1.py
+++ HW1.py
@@ -1,4 +1,16 @@
 #Name: Ashtin Gage Huntington
+
 #Class: 5th Hour
 #Assingment: HW1
 print("HELL0 WORLD")
+print("WELCOME TO AGH AI")
+print("WHAT IS YOUR NAME?")
+input("")
+print("WHY IS IT YOU ACCESSED AGH AI?")
+input("")
+print("WHAT THE THE PROBLEM YOUR HAVING TROUBLE WITH?")
+x = int(input("x = "))
+y = int(input("y = "))
+print("YOUR ANSWER IS:")
+print(f"{x + y}")
+
