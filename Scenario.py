c15d7fd21a25f74355a47c721f94328bdd3abcd6
Ashtin Gage Huntington <ah128195@stundent.wpsok.org>
2024-09-18 13:17:32 -0500

    Added Scenario

--- Scenario.py
+++ Scenario.py
@@ -12,32 +12,32 @@ videoGame = {
     "Zombies" : {
         "Attack":"65APM",
         "Size": "Medium",
-        "Health": [150],
-        "Damage": "15",},
+        "Health": 150,
+        "Damage": 15,},
 
     "Spitter" : {
         "Attack Speed" : "50AMP",
         "Size": "Medium",
-        "Health": [175],
-        "Damage": "20",},
+        "Health": 175,
+        "Damage": 20,},
 
     "Runner" : {
         "Attack Speed" : "80APM",
         "Size" : "Small",
-        "Health" : [100],
-        "Damage" : "20"  },
+        "Health" : 100,
+        "Damage" : 20  },
 
     "Brute" : {
         "Attack Speed" : "10APM",
         "Size": "Large",
-        "Health": [250],
-        "Damage": "50" },
+        "Health": 250,
+        "Damage": 50 },
 
     "Mutated" : {
         "Attack Speed" : "5APM",
         "Size": "Large",
-        "Health": [500],
-        "Damage": "200"},
+        "Health": 500,
+        "Damage": 200},
 
 }
 
@@ -45,12 +45,14 @@ videoGame = {
 print(videoGame["Zombies"]["Damage"],videoGame["Spitter"]["Damage"],videoGame["Runner"]["Damage"],videoGame["Brute"]["Damage"], videoGame["Mutated"]["Damage"])
 
 
-videoGame["Zombies"]["Damage"] = input("New Zombie Damage:")
-videoGame["Spitter"]["Damage"] = input("New Spitter Damage:")
-videoGame["Runner"]["Damage"] = input("New Runner Damage:")
-videoGame["Brute"]["Damage"] = input("New Brute Damage:")
-videoGame["Mutated"]["Damage"] = input("New Mutated Damage:")
+videoGame["Zombies"]["Damage"] = int(input("New Zombie Damage:"))
+videoGame["Spitter"]["Damage"] = int(input("New Spitter Damage:"))
+videoGame["Runner"]["Damage"] = int(input("New Runner Damage:"))
+videoGame["Brute"]["Damage"] = int(input("New Brute Damage:"))
+videoGame["Mutated"]["Damage"] = int(input("New Mutated Damage:"))
 
 
 print(videoGame["Zombies"]["Damage"],videoGame["Spitter"]["Damage"],videoGame["Runner"]["Damage"],videoGame["Brute"]["Damage"], videoGame["Mutated"]["Damage"])
-fes
+
+
+
