df3ac698e40d355ce47adc38ba72ad6c93c21343
Ashtin Gage Huntington <ah128195@stundent.wpsok.org>
2024-10-03 13:17:38 -0500

    Added HW7 (uncomplete)

---
+++ HW 7.py
@@ -0,0 +1,22 @@
+#Ashting Gage Huntington
+#5th Hour
+#HW7
+
+print(" Hello World!")
+
+print (10==9)
+
+wifi=true
+
+
+
+
+#2. Create three different boolean variables named wifi, login, and admin.
+
+#3. Create a separate integer variable that denotes the number of times
+#someone with admin credentials has logged in.
+
+#4. Create a nested if statement that checks to see if wifi is true,
+#login is true, and admin is true. If they are all true, print a
+#welcome message and increase the integer variable by one. If one of them
+#is false, print an error message telling them which one they are "missing".
