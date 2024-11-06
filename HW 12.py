d2c94f5de877f9057b43c307717e158defdadd64
Ashtin Gage Huntington <ah128195@stundent.wpsok.org>
2024-11-06 13:12:37 -0600

    HW 12 Pushed

---
+++ HW 12.py
@@ -0,0 +1,43 @@
+#Ashting Gage Huntington
+#5th Hour
+#HW12
+
+import time
+
+
+#1. Create a for loop with variable i that counts down from 5 to 1
+#and then prints "Hello World!" at the end.
+
+for i in range(5,0, -1):
+    time.sleep(1)
+    print(i)
+else:
+    print("HELLO WORLD!!!!!!!")
+
+
+#2. Create a for loop that counts up and prints only even numbers
+#between 1 and 30.
+#(HINT: Look back to HW6 on how to see if a number is divisible by another)
+
+for a in range(0, 32, 2):
+    print(a)
+
+
+#3. Create a for loop that prints 5 different animals from a list.
+
+b = ["Cat", "Sam", "Monkey", "Dog", "Deer"]
+
+for n in b:
+    print(n)
+
+
+
+#4. Create a for loop that spells out a word you input backwards.
+#(HINT: Google "How to reverse a string in Python")
+
+
+
+v = input("Input Word Here :")
+f = v[::-1]
+
+print(f)
--- Playground.py
+++ Playground.py
@@ -1,50 +1,5 @@
 #Ashtin Gage Huntington
 #5th Hour Class
 #Playground
-import random
-import time
 
-A = random.randint(1,20)
-B = random.randint(1,20)
-C = random.randint(1,20)
-
-time.sleep(1)
-print("You're alone... in a dark room..ROLL PERCEPTION")
-print(f"You roll a {A}!")
-print(A)
-if A < 10 and A > 1:
-    print("You see a jail door, with no way out...")
-    print("What do you do?")
-    print("1) Cough...")
-    print("2) Search Area")
-    print("3) Roll Luck")
-    Inn = input(":")
-    if Inn == 1:
-        print("You Got the plague...YOU'RE DEAD!!!")
-
-    if Inn == 2:
-        print("ROLL FOR IT!!!")
-        print("You roll a...")
-        print(B)
-
-    if Inn == 3:
-        print("ROLL FOR IT!!!")
-        print("You roll a...")
-        print(B)
-
-        if B == 20:
-            print("You turn into god, breaking the entire prison and escaping... YOU WIN!!!")
-
-
-if A > 10:
-    print("You see a metal jail door, and a sleeping guard with a KEY!!!")
-    print("What do you do?")
-    print("A) Steal Key")
-    print("B) Cry Loudly")
-
-
-if A == 1:
-    print("You die from a cough...")
-    print("GAME OVER")
-    exit()
 
