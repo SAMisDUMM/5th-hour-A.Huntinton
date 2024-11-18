58625eacaee2af0cb851657479d846d45c632bf1
Ashtin Gage Huntington <ah128195@stundent.wpsok.org>
2024-11-18 13:18:27 -0600

    Scenario 4 (not done) Pushed

---
+++ Scenario 4.py
@@ -0,0 +1,90 @@
+#Ashting Gage Huntington
+#5th Hour
+#Scenario 4
+import random
+print("Hello World!!!")
+a = 0
+gender = random.randint(1, 4)
+dress = random.randint(1, 6)
+shoes = random.randint(1, 4)
+ac = random.randint(1, 8)
+
+print("Welcome To Project Runway!!!!")
+
+input("Press Anything To Enter: ")
+
+x = int(input("Please Enter Number Of Players: "))
+
+print(f"You Have Selected {x} Players!!")
+
+while not a == x:
+
+    gender = random.randint(1, 4)
+    dress = random.randint(1, 6)
+    shoes = random.randint(1, 4)
+    ac = random.randint(1, 8)
+
+
+
+
+
+
+    if gender == 1:
+        print("Luxurious Female")
+
+    if gender == 2:
+        print("Short Woman")
+
+    if gender == 3:
+        print("Muscular Adult Male")
+
+    if gender == 4:
+        print("Robot?")
+
+
+    print("Wearing A....")
+
+
+    if dress == 1:
+        print("Blood Red Dress.")
+
+    if dress == 2:
+        print("Blow-Up Cheese Costume.")
+
+    if dress == 3:
+        print("Butterfly Costume.")
+
+    if dress == 4:
+        print("Gigantic Walmart Sack.")
+
+    if dress == 5:
+        print("Formal Cloths.")
+
+    if dress == 6:
+        print("Fine, Dashing Clothing.")
+
+    print("With Shoes Of...")
+
+
+    if shoes == 1:
+        print("Gold!?!?")
+
+    if shoes == 2:
+        print("Red, Beautiful Heels")
+
+    if shoes == 3:
+        print("Flip Flops")
+
+    if shoes == 4:
+        print("Nothing? They're Bearfoot....")
+
+    a += 1
+
+#Scenario 4:
+#Due to scope creep leading to high development costs, the RPG you were working on has been
+#shelved for the time being. You have been transferred to a new team working on a mobile game
+#that allows you to dress up a model and rate other models in a "Project Runway" style competition.
+
+#They want to start prototyping the rating system and are asking you to make it.
+#This prototype needs to allow the user to input the number of players, let each player rate
+#a single model from 1 to 5, and then give the average score of all of the ratings.
