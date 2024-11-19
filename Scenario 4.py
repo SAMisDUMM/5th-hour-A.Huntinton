3b9f4cd40d7ebb7a400387703ec083bdc5b045d2
Ashtin Gage Huntington <ah128195@stundent.wpsok.org>
2024-11-19 13:19:19 -0600

    Scenario 4 (not done) Pushed

--- Scenario 4.py
+++ Scenario 4.py
@@ -3,7 +3,10 @@
 #Scenario 4
 import random
 print("Hello World!!!")
+i = 0
 a = 0
+t = 0
+y = 0
 gender = random.randint(1, 4)
 dress = random.randint(1, 6)
 shoes = random.randint(1, 4)
@@ -29,54 +32,98 @@ while not a == x:
 
 
 
-    if gender == 1:
-        print("Luxurious Female")
-
-    if gender == 2:
-        print("Short Woman")
-
-    if gender == 3:
-        print("Muscular Adult Male")
-
-    if gender == 4:
-        print("Robot?")
 
 
-    print("Wearing A....")
+    while not x == 0:
+        if gender == 1:
+            print("Luxurious Female")
+
+        if gender == 2:
+            print("Short Woman")
+
+        if gender == 3:
+            print("Muscular Adult Male")
+
+        if gender == 4:
+            print("Robot?")
+
+        print("Wearing A....")
+
+        if dress == 1:
+            print("Blood Red Dress.")
+
+        if dress == 2:
+            print("Blow-Up Cheese Costume.")
+
+        if dress == 3:
+            print("Butterfly Costume.")
+
+        if dress == 4:
+            print("Gigantic Walmart Sack.")
+
+        if dress == 5:
+            print("Formal Cloths.")
+
+        if dress == 6:
+            print("Fine, Dashing Clothing.")
+
+        print("With Shoes Of...")
+
+        if shoes == 1:
+            print("Gold!?!?")
+
+        if shoes == 2:
+            print("Red, Beautiful Heels")
+
+        if shoes == 3:
+            print("Cheese")
+
+        if shoes == 4:
+            print("Nothing? They're Bearfoot....")
+
+        print("They Entered With...")
+
+        if ac == 1:
+            print("A Golden Necklace Around Their neck")
+
+        if ac == 2:
+            print("A Canned Beans?")
+
+        if ac == 3:
+            print("A Pet Rock ")
+
+        if ac == 4:
+            print("A Flaming, Ruby Ring")
+
+        if ac == 5:
+            print("A Rose")
+
+        if ac == 6:
+            print("A Cheese Basket")
+
+        if ac == 7:
+            print("A Robo Dog")
+
+        if ac == 8:
+            print("Nothing..?")
+
+        i = int(input("What Is Your Rating? (1-5)       :"))
+        if i > 5 or i < 1:
+            print("Invalid Answer, Try Again, Idoit")
+            continue
+
+        x -= 1
+        t += i
+        print(i)
+    y = t / x
+    print(y)
 
 
-    if dress == 1:
-        print("Blood Red Dress.")
-
-    if dress == 2:
-        print("Blow-Up Cheese Costume.")
-
-    if dress == 3:
-        print("Butterfly Costume.")
-
-    if dress == 4:
-        print("Gigantic Walmart Sack.")
-
-    if dress == 5:
-        print("Formal Cloths.")
-
-    if dress == 6:
-        print("Fine, Dashing Clothing.")
-
-    print("With Shoes Of...")
 
 
-    if shoes == 1:
-        print("Gold!?!?")
 
-    if shoes == 2:
-        print("Red, Beautiful Heels")
 
-    if shoes == 3:
-        print("Flip Flops")
 
-    if shoes == 4:
-        print("Nothing? They're Bearfoot....")
 
     a += 1
 
